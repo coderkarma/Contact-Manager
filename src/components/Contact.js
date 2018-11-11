09f7fc99337e76798f7e22e27a2b43b00472298b
karma <karmadrukpa11@gmail.com>
2018-11-10 18:50:36 -0800

    first commit

---
+++ src/components/Contact.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+
+ class Contact extends Component {
+   constructor(){
+     super();
+       this.state = {};
+       this.onShowClick = this.onShowClick.bind(this);
+     
+   }
+   onShowClick(){
+     console.log(this.state);
+   }
+  
+  render() {
+    // destructuring and pull contacts from contact
+    const { name, email, phone } = this.props.contact;
+    return (
+      <div className="card card-body mb-3">
+        <h4>{name} <i onClick={this.onShowClick} 
+        className="fas fa-sort-down"></i></h4>
+        <ul className="list-group">
+            <li className="list-group-item">Email: {email}</li>
+            <li className="list-group-item">Phone: {phone}</li>
+        </ul>
+      </div>
+    );
+  }
+}
+Contact.prototypes = {
+ contact:PropTypes.object.isRequired,
+ 
+};
+export default Contact
