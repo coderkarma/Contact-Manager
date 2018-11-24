389a19872333687ce75df3ab0174652774626baa
karma <karmadrukpa11@gmail.com>
2018-11-24 10:47:12 -0800

    updates

--- src/components/contacts/Contact.js
+++ src/components/contacts/Contact.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import {Link} from 'react-router-dom';
 import PropTypes from 'prop-types'
 import { Consumer } from '../../context';
 import axios from 'axios';
@@ -39,6 +40,19 @@ import axios from 'axios';
               <i className="fas fa-times" style={{cursor: "pointer", float:"right", color: 'red'}}
               onClick={this.onDeleteClick.bind(this,id, dispatch)}
               />
+
+              <Link to={`contact/edit/${id}`}>
+                <i 
+                  className="fas fa-pencil-alt"
+                  style={{
+                     cursor: 'pointer',
+                     float: 'right',
+                     color: 'black',
+                     marginRight: '1rem'
+                  }}
+                  >                  
+                </i>
+              </Link>
           </h4>
         {showContactInfo ? (
           <ul className="list-group">
